b81dd8dee4e71a2d5a8a9ac65f3875fd3ff26318
p-kessler <phillkessler@gmail.com>
2024-12-17 22:52:25 +0000

    Delete empty file

--- static/main.js
+++
@@ -1,8 +0,0 @@
-// function deleteTask(task_id) {
-//     fetch("/delete-task", {
-//         method: "POST",
-//         body: JSON.stringify({ taask_id: task_id })
-//     }).then((_res) => {
-//         window.location.href = "/home";
-//     })
-// }
